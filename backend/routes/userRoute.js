1df1c2a2f56bc2fc6be2f40745cdc4dc2e34eebc
Xuxiang Huang <xuxi.huang@hotmail.com>
2024-02-24 23:35:58 -0800

    integrated ML 2p endpoint to update user skill rating score

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -228,9 +228,10 @@ router.put('/addMatchHistory', asyncHandler(async(req, res) =>{
 
   let partnerEmail = req.body.partnerEmail; 
   let opponentEmail = req.body.opponentEmail; //Pleae passed in array format
-  let oppUser;
-  let oppUser1;
-  let oppUser2;
+  let oppUser; //single player's opponent
+  let partnerUser; //double player's partner
+  let oppUser1; //double player's opponent 1
+  let oppUser2; //double player's opponent 2
 
   //Make sure partner, opponents are valid users in the system
   if(playFormat.toLowerCase() === 'single'){
@@ -263,16 +264,16 @@ router.put('/addMatchHistory', asyncHandler(async(req, res) =>{
     if(!partnerEmail){
       return res.status(404).json({ statusCode: 404,message: "Missing your partner's email." });   
     }
-    const partnerUser = await User.find({email: partnerEmail});
+    partnerUser = await User.find({email: partnerEmail});
     if(!partnerUser || !partnerUser.length){
       return res.status(404).json({ statusCode: 404,message: "Partner user Not Found! No this user found in the system." });
     
     }
   }
 
-  //TODO: if it is single match, store opponent email
+  //if it is single match, store opponent email
   if(playFormat.toLowerCase() === 'single'){
-    //TODO: update the skill rating
+    //update the skill rating
     let yourOldSkillRating = user[0].skillRating;
     console.log("Your old score rating is, ", yourOldSkillRating);
     let opponentOldSkillRating = oppUser[0].skillRating;
@@ -297,54 +298,44 @@ router.put('/addMatchHistory', asyncHandler(async(req, res) =>{
      //update this user's skill rating    
       let skillRating =MLscoreRatingResponseJsonObj["p1EloNew"];
       console.log("user1's new skill rating, ", skillRating);
-      try{
-        console.log("user1's email, ", user[0].email);
-        await User.findOneAndUpdate(
-          { email: user[0].email }, // Filter criteria
-          { skillRating: skillRating } // Update
-   
-      );
-      }catch(e){
-        console.log("Unable to update user's skill rating record");
+      let userSkillUpdateRes = await updateUserSkillRating(user[0].email, skillRating);
+      if(userSkillUpdateRes == null){
         return res.status(404).json({statusCode: 404,message: "Unable to update user's skill rating record"});
-      }   
-      
+      }
+        
       //update this user's opponent skill rating    
       let OppSkillRating =MLscoreRatingResponseJsonObj.p2EloNew;
       console.log("user2's new skill rating, ", OppSkillRating);
-      try{
-        await User.findOneAndUpdate(
-          { email: oppUser[0].email }, // Filter criteria
-          { skillRating: OppSkillRating } // Update
+      let user2SkillUpdateRes = await updateUserSkillRating(oppUser[0].email, OppSkillRating);
+      if(user2SkillUpdateRes == null){
+        return res.status(404).json({statusCode: 404,message: "Unable to update user2's skill rating record"});
+      }
+      // try{
+      //   await User.findOneAndUpdate(
+      //     { email: oppUser[0].email }, // Filter criteria
+      //     { skillRating: OppSkillRating } // Update
    
-      );
-      }catch(e){
-        console.log('Unable to update user ', oppUser[0].email, ' skill rating record');
-        return res.status(404).json({statusCode: 404,message: e});
-      } 
+      // );
+      // }catch(e){
+      //   console.log('Unable to update user ', oppUser[0].email, ' skill rating record');
+      //   return res.status(404).json({statusCode: 404,message: e});
+      // } 
     //save match history for user 1
-    let userMatchHistoryObj = new MatchHistory({
-      "email": req.body.email,
-      "date": date,
-      "playFormat": playFormat,
-      "matchingPartners" : undefined,
-      "matchingOpponents":opponentEmail[0],
-      "yourScore": yourScore,
-      "opponentScore":opponentScore,
-    });
+    let userMatchHistoryObj = buildMatchHistoryObj(req.body.email,date,playFormat,undefined,opponentEmail,yourScore,opponentScore);
+    // new MatchHistory({
+    //   "email": req.body.email,
+    //   "date": date,
+    //   "playFormat": playFormat,
+    //   "matchingPartners" : undefined,
+    //   "matchingOpponents":opponentEmail[0],
+    //   "yourScore": yourScore,
+    //   "opponentScore":opponentScore,
+    // });
     userMatchHistoryObj.save();
-    console.log("Finish uodate user1's match history");
+    console.log("Finish update user1's match history");
 
     //save match history for user 2
-    let user2MatchHistoryObj = new MatchHistory({
-      "email": opponentEmail[0],//req.body.email,
-      "date": date,
-      "playFormat": playFormat,
-      "matchingPartners" : undefined,
-      "matchingOpponents":req.body.email,
-      "yourScore": opponentScore,//yourScore,
-      "opponentScore":yourScore//opponentScore,
-    });
+    let user2MatchHistoryObj =  buildMatchHistoryObj( opponentEmail[0],date,playFormat,undefined,req.body.email,opponentScore,yourScore);
     user2MatchHistoryObj.save();
     console.log("Finish update user2's match history");
    
@@ -352,17 +343,96 @@ router.put('/addMatchHistory', asyncHandler(async(req, res) =>{
   }
   //TODO: if it is double players match, store, partner emial, two other opponents emails
   if(playFormat.toLowerCase() === 'double' || playFormat.toLowerCase() === 'mix'){
-    let userMatchHistoryObj = new MatchHistory({
-      "email": req.body.email,
-      "date": date,
-      "playFormat": playFormat,
-      "matchingPartners" : partnerEmail,
-      "matchingOpponents":opponentEmail,
-      "yourScore": yourScore,
-      "opponentScore":opponentScore
-    });
-    userMatchHistoryObj.save();
-    //TODO: update the skill rating
+
+    //update the skill rating
+    let yourOldSkillRating = user[0].skillRating;
+    console.log("Your old score rating is, ", yourOldSkillRating);
+    let yourPartnerOldSkillRating = partnerUser[0].skillRating;
+    console.log("Your partner old score rating is, ", yourPartnerOldSkillRating);
+    let opponent1OldSkillRating = oppUser1[0].skillRating;
+    console.log("Your opponent 1 old score rating is, ", opponent1OldSkillRating);
+    let opponent2OldSkillRating = oppUser2[0].skillRating;
+    console.log("Your opponent 2 old score rating is, ", opponent2OldSkillRating);
+    let MLscoreRatingResponse;
+    await doublePlayerSkillRatingUpdate(yourOldSkillRating,yourPartnerOldSkillRating,opponent1OldSkillRating,opponent2OldSkillRating,yourScore,opponentScore)
+    .then(responseBody => {
+      MLscoreRatingResponse = responseBody;
+          return MLscoreRatingResponse;
+      })
+      .catch(error => {
+          console.error(error);
+      });
+    
+    console.log("Returning Skill rating res, ", MLscoreRatingResponse);
+    let MLscoreRatingResponseJsonObj = JSON.parse(MLscoreRatingResponse)
+
+    if(!MLscoreRatingResponse){
+      return res.status(400).json({ statusCode: 400,message: "Can not update the skill rating." });  
+    }
+    //get the new skill rating from the ML response body
+    let yourNewskillRating =MLscoreRatingResponseJsonObj["t1p1EloNew"];
+    console.log("Your new skill rating, ", yourNewskillRating);
+    let yourPartnerNewskillRating =MLscoreRatingResponseJsonObj["t1p2EloNew"];
+    console.log("Your partner new skill rating, ", yourPartnerNewskillRating);
+    let opp1UserNewskillRating =MLscoreRatingResponseJsonObj["t2p1EloNew"];
+    console.log("Your opponent 1 new skill rating, ", opp1UserNewskillRating);
+    let opp2UserNewskillRating =MLscoreRatingResponseJsonObj["t2p2EloNew"];
+    console.log("Your opponent 2 new skill rating, ", opp2UserNewskillRating);
+
+    //update the User's profile to refect the new skill rating
+    let userSkillUpdateRes1 = await updateUserSkillRating(user[0].email, yourNewskillRating);
+      if(userSkillUpdateRes1 == null){
+        return res.status(404).json({statusCode: 404,message: "Unable to update user " + user[0].email+" skill rating record"});
+      }
+    
+    let userSkillUpdateRes2 = await updateUserSkillRating(partnerUser[0].email, yourPartnerNewskillRating);
+    if(userSkillUpdateRes2 == null){
+      return res.status(404).json({statusCode: 404,message: "Unable to update user " +partnerUser[0].email +" skill rating record"});
+    }
+
+    let userSkillUpdateRes3 = await updateUserSkillRating(oppUser1[0].email, opp1UserNewskillRating);
+    if(userSkillUpdateRes3 == null){
+      return res.status(404).json({statusCode: 404,message: "Unable to update user "+ oppUser1[0].email+" skill rating record"});
+    }
+
+    let userSkillUpdateRes4 = await updateUserSkillRating(oppUser2[0].email, opp2UserNewskillRating);
+    if(userSkillUpdateRes4 == null){
+      return res.status(404).json({statusCode: 404,message: "Unable to update user "+oppUser2[0].email+" skill rating record"});
+    }
+
+    //save match history for you
+    let yourMatchHistoryObj =  buildMatchHistoryObj( req.body.email,date,playFormat,partnerUser[0].email,opponentEmail,yourScore,opponentScore);
+    yourMatchHistoryObj.save();
+    console.log("Finish update your match history");
+
+    //save match history for your partner
+    let yourPartnerMatchHistoryObj =  buildMatchHistoryObj( partnerUser[0].email,date,playFormat,req.body.email,opponentEmail,yourScore,opponentScore);
+    yourPartnerMatchHistoryObj.save();
+    console.log("Finish update your partner match history");
+
+    //save match history for your opponent 1
+    let oppArr = [req.body.email,partnerUser[0].email];
+    let yourOppo1MatchHistoryObj =  buildMatchHistoryObj( oppUser1[0].email,date,playFormat,oppUser2[0].email,oppArr,opponentScore,yourScore);
+    yourOppo1MatchHistoryObj.save();
+    console.log("Finish update your opponent 1 match history");
+
+    //save match history for you opponent 2
+    let yourOppo2MatchHistoryObj =  buildMatchHistoryObj( oppUser2[0].email,date,playFormat,oppUser1[0].email,oppArr,opponentScore,yourScore);
+    yourOppo2MatchHistoryObj.save();
+    console.log("Finish update your opponent 1 match history");
+
+
+    // let userMatchHistoryObj = new MatchHistory({
+    //   "email": req.body.email,
+    //   "date": date,
+    //   "playFormat": playFormat,
+    //   "matchingPartners" : partnerEmail,
+    //   "matchingOpponents":opponentEmail,
+    //   "yourScore": yourScore,
+    //   "opponentScore":opponentScore
+    // });
+    // userMatchHistoryObj.save();
+
     return res.status(200).json({ statusCode: 200,message: "Match history created for your "+ playFormat +" player format game." });
   }
     
@@ -457,8 +527,7 @@ const singlePlayerSkillRatingUpdate = function(p1Elo,p2Elo,s1,s2){
   console.log("Calling ML backend to get new skill rating.");
 
   const http = require('http');
-  let response;
-
+ 
   const options = {
     hostname: '127.0.0.1',
     port: 8000,
@@ -466,38 +535,22 @@ const singlePlayerSkillRatingUpdate = function(p1Elo,p2Elo,s1,s2){
     method: 'GET',
   };
 
-  return new Promise((resolve, reject) => {
-    const req = http.request(options, (res) => {
-        let responseBody = '';
+  return makeRequestForSkillRating(options,http);
+}
 
-        res.on('data', (chunk) => {
-            responseBody += chunk;
-        });
+const doublePlayerSkillRatingUpdate = function(t1p1Elo,t1p2Elo,t2p1Elo,t2p2Elo,s1,s2){
+  console.log("Calling ML backend to get new skill rating.");
 
-        res.on('end', () => {
-            resolve(responseBody);
-        });
-    });
+  const http = require('http');
+ 
+  const options = {
+    hostname: '127.0.0.1',
+    port: 8000,
+    path: '/2p?t1p1Elo='+t1p1Elo+'&t1p2Elo='+t1p2Elo+'&t2p1Elo='+t2p1Elo+'&t2p2Elo='+t2p2Elo+'&s1='+s1+'&s2='+s2, //localhost:8000/2p?t1p1Elo=1678.0&t1p2Elo=1678.0&t2p1Elo=1321.0&t2p2Elo=1321.0&s1=21&s2=0
+    method: 'GET',
+  };
 
-    req.on('error', (error) => {
-        reject(error);
-    });
-
-    req.end();
-});
-  
-  // makeRequestForSkillRating(options,http)
-  // .then(responseBody => {
-  //   response = responseBody;
-  //     console.log("line 419",response);
-  //     return responseBody;
-  // })
-  // .catch(error => {
-  //     console.error(error);
-  // });
-
-  return response;
-
+  return makeRequestForSkillRating(options,http);
 }
 
 function makeRequestForSkillRating(options, http) {
@@ -520,7 +573,40 @@ function makeRequestForSkillRating(options, http) {
 
       req.end();
   });
+};
+
+async function updateUserSkillRating(email, skillRating) {
+  let updatedUser;
+  try {
+      // Find the document you want to update and update it
+      updatedUser = await User.findOneAndUpdate(
+          { email: email }, // Filter criteria
+          { skillRating: skillRating }
+      );
+
+      console.log('Updated user:', updatedUser);
+  } catch (error) {
+      console.error('Error finding and updating record:', error);
+  }
+
+  return updatedUser;
 }
+
+const buildMatchHistoryObj = function(email, date, playFormat,matchingPartners,matchingOpponents,yourScore,opponentScore){
+  let userMatchHistoryObj = new MatchHistory({
+    "email": email,
+    "date": date,
+    "playFormat": playFormat,
+    "matchingPartners" : matchingPartners,
+    "matchingOpponents":matchingOpponents,
+    "yourScore": yourScore,
+    "opponentScore":opponentScore
+  });
+
+  return userMatchHistoryObj
+
+}
+
 
 
 module.exports = router;
